b361a8898fd4db93cc300d4652c251b3a5346a29
Yuliya_Miatlionak <Yuliya_Miatlionak@epam.com>
2020-04-09 23:25:28 +0300

    feat: add basic error handlers and packages

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { finished } = require('stream');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
@@ -11,6 +13,24 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const { method, url, body, params } = req;
+  const start = Date.now();
+
+  // eslint-disable-next-line callback-return
+  next();
+
+  finished(res, () => {
+    const ms = Date.now() - start;
+    const { statusCode } = res;
+    console.log(
+      `${method} ${url} ${JSON.stringify(params)} ${JSON.stringify(
+        body
+      )} ${statusCode} [${ms}ms]`
+    );
+  });
+});
+
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req, res, next) => {
@@ -22,9 +42,24 @@ app.use('/', (req, res, next) => {
 });
 
 app.use('/users', userRouter);
-
 app.use('/boards', boardsRouter);
-
 boardsRouter.use('/:boardId/tasks', tasksRouter);
+
+app.use((err, req, res) => {
+  if (err) {
+    console.log('ERROR:', err);
+  }
+  res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+});
+
+process.on('unhandledRejection', reason => {
+  console.error(`Unhandled rejection detected: ${reason.message}`);
+});
+
+process.on('uncaughtException', error => {
+  console.error(`captured error: ${error.message}`);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
 
 module.exports = app;
